619f0b6b1342fc7acce2ee53f10fa38355f4fdcb
julekwinn <julek.winn@gmail.com>
2024-05-27 00:53:52 +0200

    Setting up Views and URLs

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializer import MovieSerializer
+from .models import Moviedata
 
 # Create your views here.
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Moviedata.objects.all()
+    serializer_class = MovieSerializer
+
+
